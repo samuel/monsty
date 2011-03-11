669234533cfc69eddc41248e5e6464e39274315d
Samuel Stauffer <samuel@descolada.com>
2011-03-10 16:46:30 -0800

    first commit

---
+++ monsty/command.py
@@ -0,0 +1,83 @@
+
+import socket
+import time
+import yaml
+from optparse import OptionParser
+
+class CarbonClient(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = None
+    
+    def connect(self):
+        if not self.sock:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.host, self.port))
+    
+    def close(self):
+        if self.sock:
+            self.sock.close()
+            self.sock = None
+    
+    def send(self, points, timestamp=None, close=False):
+        timestamp = str(timestamp or time.time())
+        message = "\n".join(
+            "%s %s %s" % (
+                name, ("%.3f" % value) if isinstance(value, float) else value, timestamp
+            ) for name, value in points.iteritems()
+        )
+
+        self.connect()
+        self.sock.sendall(message)
+        if close:
+            self.close()
+
+def build_parser():
+    parser = OptionParser(usage="Usage: %prog [options] logfile")
+    parser.add_option("-c", "--carbon", dest="carbon_addr", help="Address of crabon/graphite (host[:port])", default=None)
+    return parser
+
+def main():
+    parser = build_parser()
+    options, args = parser.parse_args()
+    if not args:
+        parser.error("must provide path of config file")
+    
+    with open(args[0], "r") as fp:
+        config = yaml.load(fp)
+    
+    carbon_addr = config.get("carbon") or options.carbon_addr or "127.0.0.1:2003"
+    if ":" not in carbon_addr:
+        carbon_addr += ":2003"
+    host, port = carbon_addr.split(':')
+    port = int(port)
+
+    carbon = CarbonClient(host, port)
+    carbon.connect()
+    
+    name_prefix = "rad"
+    
+    for pin in config["instruments"]:
+        class_path = pin["class"]
+        mod_path, class_name = class_path.rsplit('.', 1)
+        mod = __import__(mod_path, {}, {}, [class_name])
+        cls = getattr(mod, class_name)
+        try:
+            inst = cls(**pin["args"])
+        except:
+            sys.stderr.write("Instrument %s failed\n" % pin["name"])
+            import traceback
+            traceback.print_exc()
+        else:
+            keys = pin.get('keys')
+            info = dict(
+                (".".join((name_prefix, pin["name"], k)), v)
+                for k, v in inst.get_info().iteritems()
+                if not keys or k in keys)
+            carbon.send(info)
+    
+    carbon.close()
+
+if __name__ == "__main__":
+    main()
---
+++ monsty/instruments/redislap.py
@@ -0,0 +1,38 @@
+
+import socket
+
+class RedisInstrument(object):
+    DEFAULT_INFO_KEYS = (
+        "used_cpu_sys",
+        "used_cpu_user",
+        "connected_clients",
+        "connected_slaves",
+        "blocked_clients",
+        "used_memory",
+        "used_memory_rss",
+        "mem_fragmentation_ratio",
+        "total_connections_received",
+        "total_commands_processed",
+        "expired_keys",
+        "evicted_keys",
+        "keyspace_hits",
+        "keyspace_misses",
+        "pubsub_channels",
+        "pubsub_patterns",
+    )
+    
+    def __init__(self, host, port=6379):
+        self.host = host
+        self.port = port
+
+    def get_info(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+        s.send("*1\r\n$4\r\ninfo\r\n")
+        buf = ""
+        while '\r\n\r\n' not in buf:
+            buf += s.recv(1024)
+        s.close()
+
+        info = dict(x.split(':', 1) for x in buf.split('\r\n')[1:-2])
+        return dict((key, info[key]) for key in self.DEFAULT_INFO_KEYS if key in info)
